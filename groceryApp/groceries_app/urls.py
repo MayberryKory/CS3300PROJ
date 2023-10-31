e4acd3266ea33001afa092e05b9e26683061a3ef
MayberryKory <102997911+MayberryKory@users.noreply.github.com>
2023-10-31 15:10:27 -0600

    Updated stylings

--- groceryApp/groceries_app/urls.py
+++ groceryApp/groceries_app/urls.py
@@ -4,9 +4,9 @@ from groceries_app import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('groceryItems/', views.GroceryItemListView.as_view(), name= 'grocery-items'),
-    path('groceryItems/<int:pk>', views.GroceryItemDetailView.as_view(), name='grocery-item-detail'),
+    path('groceryItems/<int:pk>', views.GroceryItemDetailView.as_view(), name='item-detail'),
     path('recipes/', views.RecipeView.as_view(), name= 'recipes'),
-    path('recipes/<int:pk>', views.RecipeDetailView.as_view(), name='recipes-detail'),
+    path('recipes/<int:pk>', views.RecipeDetailView.as_view(), name='recipe-detail'),
     path('recipeLists/', views.RecipeListListView.as_view(), name= 'recipe-lists'),
     path('recipeLists/<int:pk>', views.RecipeListDetailView.as_view(), name='recipe-lists-detail'),
 ]
