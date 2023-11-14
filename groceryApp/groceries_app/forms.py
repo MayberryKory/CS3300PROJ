9100d1f4db940929f60a854d7750f484768bcbe4
MayberryKory <102997911+MayberryKory@users.noreply.github.com>
2023-11-14 09:38:32 -0700

    adjusted templates and forms

--- groceryApp/groceries_app/forms.py
+++ groceryApp/groceries_app/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from django.forms import forms, ModelMultipleChoiceField, CheckboxSelectMultiple
+from django.forms import forms, ModelMultipleChoiceField, CheckboxSelectMultiple, SelectMultiple
 from .models import GroceryItem, Recipe, RecipeList
 from django.forms.formsets import formset_factory
 
@@ -14,8 +14,8 @@ class GroceryItemForm(ModelForm):
 class RecipeForm(ModelForm):
     grocery_items = ModelMultipleChoiceField(
         queryset=GroceryItem.objects.all(),
-        widget= CheckboxSelectMultiple,  # You can use any widget you prefer
-        required=False,  # You can set this to True if at least one item should be selected
+        widget= CheckboxSelectMultiple,  
+        required=False,  
     )
 
     class Meta:
@@ -25,7 +25,7 @@ class RecipeForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(RecipeForm, self).__init__(*args, **kwargs)
 
-        # If you want to prepopulate the form with selected items, you can do so here
+        
         if self.instance.pk:
             self.fields['grocery_items'].initial = self.instance.recipe_ingredients.all()
 
